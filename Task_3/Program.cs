b554ea9f08f1c6300b3acb7af7b7ee27c7dd0fb9
Valeriia <bartfeld.ler@yandex.ru>
2022-03-03 13:25:48 +0200

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int randomNumber = new Random().Next(-1000,1001);
+Console.WriteLine(randomNumber);
+if (randomNumber % 7 == 0 & randomNumber % 23 == 0)
+   {
+       Console.WriteLine("Это число кратно 7 и 23");
+   }
+   else
+   {
+       Console.WriteLine("Это число не кратно 7 и 23");
+   }
