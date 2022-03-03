6960f5a59f1248de018dbb2e37d97aed9c7add53
Valeriia <bartfeld.ler@yandex.ru>
2022-03-03 13:21:05 +0200

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Вторая цифра трёхзначного числа {number}");
+int a = number % 100;
+int b = a / 10;
+Console.Write($"равна {b}");
